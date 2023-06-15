513661ac076b399e6c3e687a8193a319f8a54994
RamaDerry26 <ramaderry26@gmail.com>
2023-06-15 08:24:41 +0700

    Code

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'webcrudrama';
+$databaseUsername = 'root';
+$databasePassword = '';
+ 
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+ 
+?>
---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+include_once("config.php");
+ 
+$result = mysqli_query($mysqli, "SELECT * FROM peserta ORDER BY id DESC");
+?>
+ 
+<html>
+<head>    
+    <title>Data Siswa</title>
+    <style>
+        .btn_edit{
+            color:red;
+            background: transparent;
+            border: 2px solid #0099CC;
+            border-radius: 6px;
+
+            
+            
+        }
+    </style>
+</head>
+ 
+<body>
+    <h1>DATA SISWA</h1>
+    <link rel="stylesheet" type="text/css" href="style.css"> 
+    <button onclick="window.location.href='add.php'" class="btn first">Tambah data</button><br/><br/>
+
+ 
+    <table class="tabel1" border="2" align="center" width="100%">
+ 
+    <tr>
+        <th><b>Foto</b></th>
+        <th><b>Nama</b></th>
+        <th><b>Sekolah</b></th> 
+        <th><b>Alamat</b></th> 
+        <th><b>Aksi</b></th>
+    </tr>
+    <?php  
+    while($user_data = mysqli_fetch_array($result)) {         
+        echo "<tr>";
+        echo "<td><img src='foto/".$user_data['foto']."' width='100px' height='100px'></td>";   
+        echo "<td>".$user_data['nama']."</td>";
+        echo "<td>".$user_data['sekolah']."</td>";
+        echo "<td>".$user_data['alamat']."</td>";       
+        echo "<td><a class='btn_edit' href='edit.php?id=$user_data[id]'>Edit</a> | <a href='delete.php?id=$user_data[id]'>Delete</a></td></tr>";        
+    }
+    ?>
+    </table>
+
+    
+</body>
+</html>
